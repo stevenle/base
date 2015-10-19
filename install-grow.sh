cd2db11a286c4793678c8c8eb129975659bbbd00
Steven Le <stevenle08@gmail.com>
2015-10-19 12:09:39 -0700

    Always download and install latest grow

--- install-grow.sh
+++ install-grow.sh
@@ -2,11 +2,7 @@
 
 set -ex
 
-if which grow >/dev/null; then
-  echo "Using cached grow binary"
-else
-  git clone -b develop --single-branch \
-    https://github.com/grow/pygrow.git \
-    $HOME/pygrow
-  pip install $HOME/pygrow
-fi
+git clone -b develop --single-branch \
+  https://github.com/grow/pygrow.git \
+  $HOME/pygrow
+pip install $HOME/pygrow
